a0782c025499fba56f52dea6546ce33cdca35fb8
spacerest <robconvo@gmail.com>
2019-04-22 16:14:43 +0800

    exclude image from posting to drf from client

--- client/app/serializers/artwork.js
+++ client/app/serializers/artwork.js
@@ -3,25 +3,9 @@ import DS from 'ember-data';
 
 
 export default DS.JSONAPISerializer.extend({
-  //keyForAttribute(key, method) {
-  //  return dasherize(key);
-  //}
-//workaround to deal with apparent caching of previously underscored model attr
-//names
-    //attrs: {
-    //    moonDate: 'moonDate',
-    //    moonRelativeDate: 'moonRelativeDate',
-    //    mainImageUrl: 'mainImageUrl',
-    //    mainImageInstagramUrl: 'mainImageInstagramUrl',
-    //    mainImageColor: 'mainImageColor',
-    //    maskPositiveSpaceUrl: 'maskPositiveSpaceUrl',
-    //    maskPositiveSpaceInstagramUrl: 'maskPositiveSpaceInstagramUrl',
-    //    maskPositiveSpaceColor: 'maskPositiveSpaceColor',
-    //    maskNegativeSpaceUrl: 'maskNegativeSpaceUrl',
-    //    maskNegativeSpaceInstagramUrl: 'maskNegativeSpaceInstagramUrl',
-    //    maskNegativeSpaceColor: 'maskNegativeSpaceColor',
-    //    negativeSpaceTransparency: 'negativeSpaceTransparency',
-    //    positiveSpaceTransparency: 'positiveSpaceTransparency',
-    //    dimensionality: 'dimensionality'
-    //}
+    serializeAttribute: function(record, json, key, attribute) {
+        if (attribute.name !== 'image') {
+            this._super(record, json, key, attribute);
+        }
+    }
 });
