2a917f6b188650356b2067b191baee2df451fd88
spacerest <robconvo@gmail.com>
2019-04-22 12:05:30 +0800

    add moonmask library, create hardcoded collage

--- server/art/models.py
+++ server/art/models.py
@@ -2,8 +2,17 @@ from django.db import models
 from moonmask.collage import Collage
 from api.models import User
 
+#processing and saving images files
+from django.core.files.base import ContentFile
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from io import BytesIO
+
+#for signals
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from functools import wraps
+
 # Create your models here.
-#
 
 class Artwork(models.Model):
     #using camel case for model fields because
@@ -58,3 +67,44 @@ class Artwork(models.Model):
     dimensionality = models.TextField(max_length=3,
                                       null=True,
                                       blank=True)
+
+
+#very helpful thing about decorators and skipping signals
+#https://stackoverflow.com/a/27161229/5650506
+def skip_signal():
+    def _skip_signal(signal_func):
+        @wraps(signal_func)
+        def _decorator(sender, instance, **kwargs):
+            if hasattr(instance, 'skip_signal'):
+                return None
+            return signal_func(sender, instance, **kwargs)
+        return _decorator
+    return _skip_signal
+
+# method for updating
+@receiver(post_save, sender=Artwork, dispatch_uid="make_image")
+@skip_signal()
+def make_image(sender, instance, **kwargs):
+    print("starting make image")
+    c = Collage()
+    c.set_mask()
+    c.set_main_image("pink", color="pink")
+    c.set_mask_positive_space("yellow", color="yellow")
+    c.set_mask_negative_space("black", color="black")
+    c.create_collage()
+    im = c.get_created_collage()
+    file_name=instance.title + ".jpg"
+    buffer = BytesIO()
+    im.save(fp=buffer, format='PNG')
+    pillow_image = ContentFile(buffer.getvalue())
+    print("saving...")
+    instance.skip_signal = True
+    instance.image.save(file_name, InMemoryUploadedFile(
+                pillow_image,
+                None,
+                file_name,
+                'image/jpeg',
+                pillow_image.tell,
+                None
+    ))
+    print("finished with maek iamge")
--- server/art/views.py
+++ server/art/views.py
@@ -14,6 +14,13 @@ class artworkAPIView(mixins.CreateModelMixin, generics.ListAPIView):
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
+class artworkUpdateAPIView(mixins.UpdateModelMixin, generics.ListAPIView):
+    resource_name = 'artworks'
+    serializer_class = artworkSerializer
+
+    def post(self, request, *args, **kwargs):
+        return self.save(request, *args, **kwargs)
+
 class artworkRudView(generics.RetrieveUpdateDestroyAPIView):
     #permission_classes = (IsAuthenticated, )
     resource_name = 'artworks'
